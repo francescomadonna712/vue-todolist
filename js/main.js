5537c4fa2d4bffe616c676ab4d7c2163a4385b0e
francesco madonna <fra.mad.93@hotmail.it>
2024-05-14 17:33:59 +0200

    creazione lista

--- js/main.js
+++ js/main.js
@@ -3,7 +3,22 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            list: [
+                {
+                    elemento: 'case',
+                },
+                {
+                    elemento: 'libri',
+                },
+                {
+                    elemento: 'auto',
+                },
+                {
+                    elemento: 'fogli di giornale',
+                },
+            ],
+            inputText: '',
+            
         }
     }
 }).mount('#app')
